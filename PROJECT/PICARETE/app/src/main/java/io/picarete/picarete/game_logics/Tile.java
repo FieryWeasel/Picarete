476f2f6201beef73df32498b31fdc6192c0ee59e
SJ <j.simon01.c@gmail.com>
2015-01-07 11:56:00 +0100

    Add IA logics and add some missing piece of code

--- PROJECT/PICARETE/app/src/main/java/io/picarete/picarete/game_logics/Tile.java
+++ PROJECT/PICARETE/app/src/main/java/io/picarete/picarete/game_logics/Tile.java
@@ -2,6 +2,7 @@ package io.picarete.picarete.game_logics;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Point;
 import android.os.Debug;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -16,10 +17,14 @@ import java.util.Map;
  */
 
 public class Tile extends ImageView implements View.OnTouchListener{
-    Map<ETileSide, Edge> edges;
+    private Map<ETileSide, Edge> edges;
 
     // Event Management
     TileEventListener eventListener = null;
+
+    public Map<ETileSide, Edge> getEdges() {
+        return edges;
+    }
 
     public interface TileEventListener {
         public abstract void OnClick(Tile l, Edge edge);
@@ -45,6 +50,12 @@ public class Tile extends ImageView implements View.OnTouchListener{
     public boolean onTouch(View v, MotionEvent event) {
         // Todo Detect the Edge clicked
         Edge edge = null;
+        MotionEvent.PointerCoords coor = new MotionEvent.PointerCoords();
+        event.getPointerCoords(0, coor);
+        float posX = coor.x;
+        float posY = coor.y;
+
+        edge = choseEdge(posX, posY);
 
         if(!edge.isChosen()){
             edge.setChosen(true);
@@ -56,6 +67,52 @@ public class Tile extends ImageView implements View.OnTouchListener{
 
 
         return true;
+    }
+
+    private Edge choseEdge(float posX, float posY) {
+        // http://math.stackexchange.com/questions/51326/determining-if-an-arbitrary-point-lies-inside-a-triangle-defined-by-three-points
+        Edge edge = null;
+        int width = 100, height = 100;
+
+        Point p1 = new Point();
+        Point p2 = new Point();
+        Point p3 = new Point();
+        Point p4 = new Point((int) posX, (int) posY);
+
+        // Create points
+        p1 = new Point(0, 0);
+        p2 = new Point(width, 0);
+        p3 = new Point(width/2, height/2);
+
+        // Move the origin at one vertex
+        p2 = moveToPoint(p2, p1);
+        p3 = moveToPoint(p3, p1);
+        p4 = moveToPoint(p4, p1);
+
+        // Calculate the scalar d
+        float d = p2.x*p3.y - p3.x*p2.y;
+        // Calculate Barycentric weights
+        //float w1 = p4.x(p2.y-p3.y) + p4.y
+
+        if(posX > 0 && posX < width/2 && posY > 0 && posY < height)
+            edge = edges.get(ETileSide.LEFT);
+        else if(posX > 0 && posX < width && posY > 0 && posY < height/2)
+            edge = edges.get(ETileSide.TOP);
+        else  if(posX > width/2 && posX < width && posY > 0 && posY < height)
+            edge = edges.get(ETileSide.RIGHT);
+        else if(posX > 0 && posX < width && posY > height/2 && posY < height)
+            edge = edges.get(ETileSide.BOTTOM);
+
+        return edge;
+    }
+
+    private Point moveToPoint(Point pointToMove, Point origin){
+        Point p = new Point();
+
+        p.x = pointToMove.x - origin.x;
+        p.y = pointToMove.y - origin.y;
+
+        return p;
     }
 
     public boolean isComplete(){
