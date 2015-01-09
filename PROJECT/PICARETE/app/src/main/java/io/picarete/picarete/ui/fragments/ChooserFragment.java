beb85f974255a9d084fd104b80d3d770aa3e3054
FieryWeasel <sayanael@gmail.om>
2015-01-09 11:38:25 +0100

    Mergin SoloChooser/MultiChooser to avoid code duplicate

---
+++ PROJECT/PICARETE/app/src/main/java/io/picarete/picarete/ui/fragments/ChooserFragment.java
@@ -0,0 +1,80 @@
+package io.picarete.picarete.ui.fragments;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.picarete.picarete.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ChooserFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ChooserFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public abstract class ChooserFragment extends Fragment {
+
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     */
+    public abstract ChooserFragment newInstance();
+
+    public ChooserFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chooser, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+
+        public void onFragmentInteraction();
+    }
+
+}
